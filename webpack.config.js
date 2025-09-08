61ab98cc6bd6df8eafada24ee2fe61903ac8fefe
Md. Touhid Alam <touhidalam69@gmail.com>
2025-09-08 15:13:20 +0600

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+};
